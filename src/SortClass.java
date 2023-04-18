cb329f0762c4fb0c63c489859085aa3c05ed04fc
varx0 <ben.harold@protonmail.ch>
2023-04-18 18:32:07 -0500

    There's a snake in my boot

--- src/SortClass.java
+++ src/SortClass.java
@@ -1,8 +1,5 @@
 import javax.swing.*;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -33,27 +30,52 @@ public class SortClass {
         JOptionPane.showInputDialog(Builder);
 
     }
-    public static void sortUID(ArrayList<Users> users){
-        ArrayList<String> names = new ArrayList<>();
-        String Builder = "";
-        for(int i = 0; i<users.size(); i++) {
-            names.add(users.get(i).getUID() +"," + users.get(i).getUsername());
+    public static void sortUID(ArrayList<Users> users)throws IOException{
+        File input = new File("list.txt");
+        File output = new File("temp.txt");
+        File backup = new File("list.bak");
+        BufferedWriter writes = new BufferedWriter(new FileWriter(output));
+        String currentLine;
+
+        int[] result = new int[users.size()];
+
+        int temp;
+        int iterate;
+        for(int i = 0; i<users.size(); i++){
+            result[i] = users.get(i).getUID();
         }
-        Collections.sort(names);
-        for(int i = 0; i <names.size(); i++){
-            String[] split = names.get(i).split(",");
-            Builder = Builder + "UID: " + split[0] +", " + "Username: " + split[1];
-            Builder = Builder + "\n";
+
+        for(int i = 0; i < result.length; i++){
+            for(int y = 0; y < result.length-1; y++){
+                if(result[y+1] < result[y]){
+                    temp = result[y+1];
+                    result[y+1] = result[y];
+                    result[y] = temp;
+                }
+            }
         }
-        JOptionPane.showInputDialog(Builder);
+
+        for(int i = 0; i < result.length; i++){
+            BufferedReader reads2 = new BufferedReader(new FileReader(input));
+            currentLine = reads2.readLine();
+            String trimmed = currentLine.trim();
+            String[] split = trimmed.split(",");
+            if(result[i] == Integer.valueOf(split[3])){
+                writes.write(currentLine);
+            }
+
+        }
+
     }
 
     public static void sortGroup(ArrayList<Users> users){
         ArrayList<Users> sorted = new ArrayList<>();
         while(sorted.size() < users.size()){
             for(int i =0; i < users.size(); i++){
-                if(users.get(i).getgroups() == "Admin"){
-                    users.get(i);
+                for(int y = i+1; i < users.size(); y++){
+                    if(users.get(i).getUID() > users.get(i).getUID()){
+
+                    }
                 }
             }
         }
